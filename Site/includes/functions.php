cd10bc90ff65fa2bc0e5177a098b1cd43cc4ef17
SivaOnOffVM <sivadarshan@gmail.com>
2025-07-13 16:12:17 +0530

    Commit

--- Site/includes/functions.php
+++ Site/includes/functions.php
@@ -93,7 +93,7 @@ function is_logged_in() {
 function require_login() {
     if (!is_logged_in()) {
         $_SESSION['login_redirect'] = $_SERVER['REQUEST_URI'];
-        header('Location: login.php');
+        header('Location: /login.php');
         exit;
     }
 
