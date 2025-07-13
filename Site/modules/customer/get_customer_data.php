433c59fa7e16b750c2b23bddaa15d3bb09af4d74
SivaOnOffVM <sivadarshan@gmail.com>
2025-07-13 06:49:10 +0530

    Commit

--- Site/modules/customer/get_customer_data.php
+++ Site/modules/customer/get_customer_data.php
@@ -1,16 +1,20 @@
 <?php
+
 // File: /modules/customer/get_customer_data.php
+
+header('Content-Type: application/json');
+define('_IN_APP_', true);
 require_once __DIR__ . '/../../config/db.php';
 require_once __DIR__ . '/../../includes/functions.php';
 
-header('Content-Type: application/json');
-
 if (!isset($_GET['customer_id'])) {
-    echo json_encode(['error' => 'Customer ID required']);
+    echo json_encode(['error' => 'Missing customer_id']);
     exit;
 }
 
-$customer = get_customer($_GET['customer_id']);
+$customer_id = $_GET['customer_id'];
+$customer = get_customer($customer_id);
+
 if (!$customer) {
     echo json_encode(['error' => 'Customer not found']);
     exit;
