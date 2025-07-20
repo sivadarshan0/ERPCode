7550b68dbacbda55bfc9fbb7f820a4f11de342e0
SivaOnOffVM <sivadarshan@gmail.com>
2025-07-20 09:33:13 +0530

    Commit

--- Site/modules/customer/entry_customer.php
+++ Site/modules/customer/entry_customer.php
@@ -5,9 +5,9 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 define('_IN_APP_', true);
 
-define('_IN_APP_', true);
 require_once __DIR__ . '/../../config/db.php';
 require_once __DIR__ . '/../../includes/functions.php';
+
 require_login();
 
 $page_title = "Customer Entry";
